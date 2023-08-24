925c87656c9e9cb58591a9aaa188623562ceb598
Paul Triebel <paulmagnus05@gmail.com>
2023-08-24 12:15:56 +0200

    Fix exit hook

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -5,7 +5,7 @@ import DataBase from "./db/db_connection";
 import MqttDataWorker from "./mqtt/mqtt_dataworker";
 import {AddressExtractor} from "./utils/address_extractor";
 import {Logger} from "./utils/logger";
-import {onExit} from "signal-exit";
+import onExit from "async-exit-hook";
 
 dotenv.config();
 Logger.createLogger(process.env.LEVEL ?? "info");
@@ -22,10 +22,10 @@ if (AddressExtractor.extract()) {
     const api = new TempiAPI(3000, database, dataWorker, 12666);
     api.serve();
 
-    onExit(async signal => {
-        await database.allSensorsDead();
-        Logger.info("Stopped")
-    });
+    onExit(resolve => database.allSensorsDead().finally(() => {
+        Logger.info("Stopped");
+        resolve();
+    }));
 } else {
     Logger.error("Error on Address Extraction");
 }
