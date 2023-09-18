2394399b26f3b15adc723a80548a030ae1a529df
Paul Triebel <paulmagnus05@gmail.com>
2023-09-18 13:47:23 +0200

    Add timeline interpolation

--- src/frontend/src/main/overview/TemperatureOverview.jsx
+++ src/frontend/src/main/overview/TemperatureOverview.jsx
@@ -1,13 +1,14 @@
-import {Card, Col, Grid, LineChart, Metric, Title} from "@tremor/react"
+import {Card, Col, Grid, LineChart, Metric, Tab, TabList, Title} from "@tremor/react"
 import {addHours, subDays, subHours, subMinutes} from "date-fns"
 import SensorSettings from "./SensorSettings"
 import {getSensorDataTimeline} from "../../api/api"
 import RealtimeComponent from "../../utils/RealtimeComponent"
+import {remap} from "../../utils/MathUtils"
 
 export default class TemperatureOverview extends RealtimeComponent {
 	constructor(props) {
 		super(props)
-		this.state = {chartData: [], filter: "Max"}
+		this.state = {chartData: [], filter: "M"}
 	}
 
 	componentDidMount() {
@@ -15,7 +16,7 @@ export default class TemperatureOverview extends RealtimeComponent {
 		getSensorDataTimeline(this.props.sensor.uuid, "temp").then((data) =>
 			this.setState({
 				chartData: data.map((dataEntry) => {
-					dataEntry.timestamp = addHours(new Date(dataEntry.timestamp), 2)
+					dataEntry.timestamp = new Date(dataEntry.timestamp)
 					return dataEntry
 				})
 			})
@@ -53,8 +54,6 @@ export default class TemperatureOverview extends RealtimeComponent {
 				case "W":
 					periodStartDate = subDays(lastAvailableDate, 7)
 					break
-				default:
-					periodStartDate = lastAvailableDate
 			}
 			if (periodStartDate) {
 				return this.state.chartData.filter(
@@ -68,53 +67,65 @@ export default class TemperatureOverview extends RealtimeComponent {
 	}
 
 	render() {
-		const filteredData = this.getFilteredData(this.state.filter).map((dataEntry) => {
-			dataEntry.timestamp = dataEntry.timestamp.toLocaleString()
-			return dataEntry
-		})
+		let filteredData = this.getFilteredData(this.state.filter)
+		let lastValue
+		filteredData = filteredData.reduce((data, value) => {
+			if (lastValue) {
+				const lastValueTime = lastValue.timestamp.getTime() / 60000
+				const valueTime = value.timestamp.getTime() / 60000
+				for (let i = lastValueTime + 1; i <= valueTime; i++) {
+					const x = remap(i, lastValueTime, valueTime, 0, 1)
+					const y = (Math.cos(Math.PI * x) + 1) / 2
+					data.push({
+						Time: new Date(i * 60000).toLocaleTimeString(),
+						Temperature: remap(y, 1, 0, lastValue.value, value.value)
+					})
+				}
+			} else {
+				data.push({
+					Time: value.timestamp.toLocaleTimeString(),
+					Temperature: value.value
+				})
+			}
+			lastValue = value
+			return data
+		}, [])
+
+		console.log(filteredData)
 		return (
-			<Grid className="mt-6" numCols="2">
-				<Col className="m-2">
-					<Card>
-						<Title>
-							<Metric>
-								{filteredData.length > 0 ? filteredData[filteredData.length - 1].value : null}
-								°C
-							</Metric>
-							Temperature Chart
-						</Title>
-						{/*
-                    <TabList
-                        defaultValue={this.state.filter}
-                        onValueChange={value => this.setState({filter: value})}
-                        className="mt-10"
-                    >
-                        <Tab value="M" text="Minute"/>
-                        <Tab value="H" text="Hour"/>
-                        <Tab value="D" text="Day"/>
-                        <Tab value="W" text="Week"/>
-                        <Tab value="Max" text="Max"/>
-                    </TabList>
-                    */}
-						<LineChart
-							className="mt-6"
-							data={filteredData}
-							index="timestamp"
-							categories={["value"]}
-							colors={["blue"]}
-							showAnimation={true}
-							showLegend={false}
-							showGradient={true}
-							valueFormatter={this.dataFormatter}
-							minValue={-30}
-							maxValue={60}
-						/>
-					</Card>
-				</Col>
-				<Col className="m-2">
-					<SensorSettings sensor={this.props.sensor} settings={this.props.settings} />
-				</Col>
-			</Grid>
+			<Card>
+				<Title>
+					<Metric>
+						{filteredData.length > 0 ? filteredData[filteredData.length - 1].Temperature : null}
+						°C
+					</Metric>
+					Temperature Chart
+				</Title>
+				<TabList
+					defaultValue={this.state.filter}
+					onValueChange={(value) => this.setState({filter: value})}
+					className="mt-10"
+				>
+					<Tab value="M" text="Minute" />
+					<Tab value="H" text="Hour" />
+					<Tab value="D" text="Day" />
+					<Tab value="W" text="Week" />
+					<Tab value="A" text="All" />
+				</TabList>
+				<LineChart
+					className="mt-6"
+					data={filteredData}
+					index="Time"
+					categories={["Temperature"]}
+					colors={["blue"]}
+					showAnimation={false}
+					showLegend={false}
+					showGradient={true}
+					valueFormatter={(v) => Intl.NumberFormat("de", {unit: "celsius", style: "unit"}).format(v).toString()}
+					minValue={-30}
+					maxValue={60}
+				/>
+			</Card>
 		)
 	}
 }
