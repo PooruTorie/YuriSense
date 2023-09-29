fafa075357fd00c9239d75fee0a68fa03bda6a0f
Paul Triebel <paulmagnus05@gmail.com>
2023-09-29 12:39:20 +0200

    Add https

--- src/frontend/serve/index.js
+++ src/frontend/serve/index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const createProxyMiddleware = require("http-proxy-middleware")
+const fs = require("fs")
+const https = require("https")
 
 const app = express()
 
@@ -20,4 +22,19 @@ app.get("*", (req, res) => {
 	res.sendFile("index.html", {root: "build"})
 })
 
-app.listen(3000)
+if (fs.existsSync("ssl/server.crt") && fs.existsSync("ssl/server.key")) {
+	https
+		.createServer(
+			{
+				key: fs.readFileSync("ssl/server.key"),
+				cert: fs.readFileSync("ssl/server.crt")
+			},
+			app
+		)
+		.listen(4000, () => {
+			console.log("HTTPS Started on 4000")
+		})
+}
+app.listen(3001, () => {
+	console.log("HTTP Started on 3000")
+})
